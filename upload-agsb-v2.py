e85a865c75d74e8b4369397bc71beb9637299e08
g8wholxg <g8wholxg@abbmail.top>
2025-06-08 00:09:05 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -33,9 +33,10 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "leoung"         # 用户名
 UUID =                       # UUID，留空则自动生成
-PORT = 0                   # Vmess端口，留空或0则自动生成
+PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN =                     # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiMzJjZTM3M2M1NDU2MmRkMDllOTU2ZGJhZjZlYzg2YWYiLCJ0IjoiZTdhNTMxNjEtYzMwZC00MzE4LTkyOGEtNjkyNGFlMTkzNTg3IiwicyI6Ik4yUm1OR1V6T0dZdE9ETXdZaTAwTkdVNUxXRTNPVFF0T0daaE5UazBaRE5qT1dRMyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+
 # =========================================
 
 # 添加命令行参数解析
